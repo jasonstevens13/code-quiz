4d0e8fa39192d73210f6c61cf567e16d56f57c82
jasonstevens13 <jasonstevens13@yahoo.com>
2020-03-22 22:42:16 -0400

    making this whole thing better

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,105 @@
 var timeEl = document.getElementById("timer");
-var startQuiz = document.getElementById("startBtn");
-var cardTitle = document.getElementById("cardTitle");
-var cardText = document.getElementById("cardText");
-var btnList = document.getElementById("btnList");
-var q1 = document.getElementById("q1");
-var correctAnswers = document.getElementById("correct");
-var incorrectAnswers = document.getElementById("incorrect");
-var displayedCard = document.getElementById("0");
+var startQuizBtn = document.getElementById("startBtn");
+var startQuiz = document.getElementById("startQuiz");
+var quiz = document.getElementById("quiz");
+var doneQuiz = document.getElementById("doneQuiz");
+var question = document.getElementById("question");
+var questionText = document.getElementById("questionText");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
+var currentQuestionIndex = 0;
 var score = 0;
 var secondsLeft = 60;
+
+
+
+// var cardTitle = document.getElementById("cardTitle");
+// var cardText = document.getElementById("cardText");
+// var btnList = document.getElementById("btnList");
+// var q1 = document.getElementById("q1");
+// var correctAnswers = document.getElementById("correct");
+// var incorrectAnswers = document.getElementById("incorrect");
+// var displayedCard = document.getElementById("0");
+
+
+
+
 var questionsArray = [
-    ['1. What is the abbreviation for Javascrip?', 'JVS', 'JS', 'JavS', 'JVscript'],
-    ['2. Javascript is linked to an html page with what tag type?', '<p>', '<html>', '<src>', '<script>'],
-    ['3. Fill in the blank. Javascript has 3 kinds of popup boxes: Alert, Confirm, and _______.', 'window', 'Warning!', 'Prompt', 'Caution!'],
-    ['4. Fill in the blank. Written in global scope, the method alert(); is the same as writing window._____();', 'alert', 'this', 'prompt', 'document'],
-    ['5. Fill in the blank. Combined, HTML, CSS, and Javascript are the languages of the ____.', 'back-end', 'server', 'programmer', 'web'],
+    {
+        question: 'What is the abbreviation for Javascrip?',
+        choiceA: 'JVS',
+        choiceB: 'JS',
+        choiceC: 'JavS',
+        choiceD: 'JVscript',
+        correct: 'B'
+    },
+    {
+        question: 'Javascript is linked to an html page with what tag type?',
+        choiceA: '<p>',
+        choiceB: '<html>',
+        choiceC: '<src>',
+        choiceD: '<script>',
+        correct: 'D'
+    },
+    {
+        question: 'Fill in the blank. Javascript has 3 kinds of popup boxes: Alert, Confirm, and _______.',
+        choiceA: 'window',
+        choiceB: 'Warning!',
+        choiceC: 'Prompt',
+        choiceD: 'Caution!',
+        correct: 'C',
+    },
+    {
+        question: 'Fill in the blank. Written in global scope, the method alert(); is the same as writing window._____();',
+        choiceA: 'alert',
+        choiceB: 'this',
+        choiceC: 'prompt',
+        choiceD: 'document',
+        correct: 'A'
+    },
+    {
+        question: 'Fill in the blank. Combined, HTML, CSS, and Javascript are the languages of the ____.',
+        choiceA: 'back-end',
+        choiceB: 'server',
+        choiceC: 'programmer',
+        choiceD: 'web',
+        correct: 'D'
+    }
 ];
+
+var lastQuestionIndex = questionsArray.length - 1;
+
+function renderQuestion() {
+    let q = questionsArray[currentQuestionIndex];
+    question.textContent = q.question;
+    questionText.textContent = q.questionText;
+    choiceA.textContent = q.choiceA;
+    choiceB.textContent = q.choiceB;
+    choiceC.textContent = q.choiceC;
+    choiceD.textContent = q.choiceD;
+}
+
+function checkAnswer(answer) {
+    if (questionsArray[currentQuestionIndex].correct == answer) {
+        score = score + 10;
+    } else {
+        console.log('Wrong');
+        secondsLeft = secondsLeft - 10;
+    }
+    if (currentQuestionIndex < lastQuestionIndex) {
+        currentQuestionIndex++;
+        renderQuestion();
+    } else {
+        clearInterval(timerInterval):
+        finishMessage();
+        scoreRender();
+    }
+
+}
+
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -35,57 +119,23 @@ function sendMessage() {
     return;
 }
 
+function finishMessage() {
+    timeEl.textContent = "You're Done!";
+    return;
+}
+
 
 
 startQuiz.addEventListener("click", function (event) {
     event.preventDefault();
-    nextCard();
     setTime();
-
-});
-
-correctAnswers.addEventListener("click", function (event) {
-    event.preventDefault();
-    score = score + 10;
-});
-
-incorrectAnswers.addEventListener("click", function (event) {
-    event.preventDefault();
-    score = score - 10;
-    secondsLeft = secondsLeft - 10;
+    renderQuestion();
 });
 
 
 
-function nextCard(displayedCard) {
-    if (displayedCard = '0') {
-        displayedCard.style.display = "none";
-        displayedCard = '1';
-        displayedCard.style.display = "block";
-    }
-    if (displayedCard = '1') {
-        displayedCard.style.display = "none";
-        displayedCard = '2';
-        displayedCard.style.display = "block";
-    };
-    if (displayedCard = '2') {
-        displayedCard.style.display = "none";
-        displayedCard = '3';
-        displayedCard.style.display = "block";
-    };
-    if (displayedCard = '3') {
-        displayedCard.style.display = "none";
-        displayedCard = '4';
-        displayedCard.style.display = "block";
-    };
-    if (displayedCard = '4') {
-        displayedCard.style.display = "none";
-        displayedCard = '5';
-        displayedCard.style.display = "block";
-    };
 
 
 
-}
 
 
